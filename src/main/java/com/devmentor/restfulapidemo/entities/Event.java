b67e2b022916e86e5cc580b47ad0a53fd8e0c1ec
Jian Marcus <renyou815@gmail.com>
2024-01-17 20:46:01 +0800

    feat(DB): JDBC SQLservier CRUD

--- src/main/java/com/devmentor/restfulapidemo/entities/Event.java
+++ src/main/java/com/devmentor/restfulapidemo/entities/Event.java
@@ -1,16 +1,53 @@
 package com.devmentor.restfulapidemo.entities;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.util.Date;
 
-@Entity
-@Table(name = "events")
 public class Event {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name") private String name;
-    @Column(name = "trigger_time") private Date triggerTime;
-    @Column(name = "create_at") private Date createdAt;
+    private String name;
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:s")
+    private Date triggerTime;
+    private Date createdAt;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getTriggerTime() {
+        return triggerTime;
+    }
+
+    public void setTriggerTime(Date triggerTime) {
+        this.triggerTime = triggerTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateAt() {
+        return createdAt;
+    }
+
+    public void setCreateAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "name='" + name + '\'' +
+                ", triggerTime=" + triggerTime +
+                '}';
+    }
 }
